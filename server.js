f75f5c512fd561e4b20118496a8ccd392eaeae15
Kristall96 <erkandev@outlook.com>
2025-04-01 09:24:12 +0300

    updated cors

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+
 dotenv.config();
+
 const app = express();
-const allowedOrigins = ["http://localhost:5173", "https://mugmarvel.store"];
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mugmarvel.store",
+  "https://backend-softuni.onrender.com",
+];
 
 app.use(
   cors({
@@ -23,16 +30,19 @@ app.use(
 );
 
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`✅ Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 });
